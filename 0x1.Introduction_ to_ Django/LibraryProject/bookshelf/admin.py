bee4b554156bcc705d138b2452e70fba1f69f77e
girmameb <girmamebratu>
2024-08-11 00:21:38 +0300

    update file

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
+from .models import Book
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # List display configuration
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Search fields configuration
+    search_fields = ('title', 'author')
+    
+    # List filter configuration
+    list_filter = ('publication_year',)
+
