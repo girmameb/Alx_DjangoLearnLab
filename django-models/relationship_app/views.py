c7c332b70c8e00e4a2cb052bdc112f67a60e7d11
girmameb <girmamebratu>
2024-08-16 22:47:08 +0300

    update file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Book  # Assuming you have a Book model
+from django.shortcuts import render, get_object_or_404
+from .models import Library
+
 
 def list_books(request):
     # Query the database for all books
@@ -7,3 +10,12 @@ def list_books(request):
 
     # Pass the books to the template
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# relationship_app/views.py
+
+def library_detail(request, library_id):
+    # Fetch the Library instance or return a 404 if not found
+    library = get_object_or_404(Library, id=library_id)
+    
+    # Render the library_detail.html template with the library context
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
