3db9b86d0020949c5e52edb7cfd49a05d4628d00
girmameb <girmamebratu>
2024-08-16 23:42:10 +0300

    update file

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,16 +2,16 @@
 
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.urls import path
+from .views import register
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 # relationship_app/urls.py
-
-from django.urls import path
-from .views import register
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
