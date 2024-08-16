08b289f2da3e2e8f00c9aade3f4e8273599d6a16
girmameb <girmamebratu>
2024-08-16 22:06:07 +0300

    update file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,7 @@
+# relationship_app/views.py
 from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
-
