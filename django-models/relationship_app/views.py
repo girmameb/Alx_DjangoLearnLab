ae41176187973e72f83bcf9bcab32e6f60c06c47
girmameb <girmamebratu>
2024-08-16 22:41:24 +0300

    update file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,9 @@
-# relationship_app/views.py
-
 from django.shortcuts import render
-from .models import Book
+from .models import Book  # Assuming you have a Book model
 
-def book_list(request):
+def list_books(request):
+    # Query the database for all books
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+
+    # Pass the books to the template
+    return render(request, 'relationship_app/list_books.html', {'books': books})
