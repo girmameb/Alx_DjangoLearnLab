11d73705b3492e47eb303bb47f98e6f1c2f8559a
girmameb <girmamebratu>
2024-08-17 20:49:21 +0300

    update file

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import register, list_books, LibraryDetailView
 
 urlpatterns = [
     # Login view with a custom template
-    path('login/', auth_views.LoginView.as_view(template_name='registration.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='register.html'), name='login'),
 
     # Logout view with redirect to login page
     #path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
@@ -12,7 +12,7 @@ urlpatterns = [
 
 
     # Registration view
-    path('register/', register(template_name='registration/register.html'), name='register'),
+    path('register/', register(template_name='register.html'), name='register'),
 
     # Books list view
     path('books/', list_books, name='list_books'),
