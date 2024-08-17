0365e660e5350c0b5d7f55340b77662a67202a43
girmameb <girmamebratu>
2024-08-17 23:57:50 +0300

    update file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -53,4 +53,4 @@ def librarian_view(request):
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'relationship_app//member_view.html')
+    return render(request,'relationship_app//member_view.html')
