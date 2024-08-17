f312ba8685c6c39d64a0c380210a629fdd9b8aa1
girmameb <girmamebratu>
2024-08-18 00:28:59 +0300

    update file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -62,33 +62,33 @@ def member_view(request):
 @permission_required('books.can_add_book', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = BookForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect to a list of books or another relevant page
+            return redirect('book_list')
     else:
         form = BookForm()
 
-    return render(request, 'relationship_app/add_book.html', {'form': form})
+    return render(request, 'books/add_book.html', {'form': form})
 
 @permission_required('books.can_change_book', raise_exception=True)
 def update_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = BookForm(request.POST, request.FILES, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect to a list of books or another relevant page
+            return redirect('book_list')
     else:
         form = BookForm(instance=book)
 
-    return render(request, 'relationship_app/update_book.html', {'form': form})
+    return render(request, 'books/update_book.html', {'form': form})
 
 @permission_required('books.can_delete_book', raise_exception=True)
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         book.delete()
-        return redirect('book_list')  # Redirect to a list of books or another relevant page
+        return redirect('book_list')
 
     return render(request, 'relationship_app/delete_book.html', {'book': book})
