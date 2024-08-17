714fbc5d635422684678d69c94b1598c8be76772
girmameb <girmamebratu>
2024-08-17 22:31:36 +0300

    update file

--- django-models/relationship_app/admin.py
+++ django-models/relationship_app/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
