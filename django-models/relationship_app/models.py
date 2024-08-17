119abb7f5760673c64e1b81db8ff524ac4ef63af
girmameb <girmamebratu>
2024-08-17 22:41:05 +0300

    update file

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -37,3 +41,12 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.name
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
