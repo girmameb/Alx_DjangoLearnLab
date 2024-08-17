93d3a9e43de5ab3554189890caf8f45077e57f60
girmameb <girmamebratu>
2024-08-17 13:23:36 +0300

    update file

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,15 +1,14 @@
 # relationship_app/urls.py
-
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
     # URL pattern for user login with a custom template
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name ='login'),
 
     # URL pattern for user logout, optionally with a custom template
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view (next_page = 'login'), name ='logout'),
 
     # URL pattern for user registration
     path('register/', register, name='register'),
