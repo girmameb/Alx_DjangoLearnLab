577364c564a2609ee27ca24c9da29c5a9069617e
girmameb <girmamebratu>
2024-08-17 23:32:05 +0300

    update file

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -24,7 +24,10 @@ path('registration/', views.register, name='register'),
     # Library detail view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
- path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+path('admin/', views.admin_view, name='admin_view'),
+path('librarian/', librarian_view, name='librarian_view'),
+path('member/', member_view, name='member_view'),
+path('admin/', admin_view, name='admin_view'),
+path('librarian/', librarian_view, name='librarian_view'),
+path('member/', member_view, name='member_view'),
 ]
