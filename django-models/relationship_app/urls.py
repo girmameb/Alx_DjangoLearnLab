9b1079899a35622ac4a93e5ca2de23a623cb0555
girmameb <girmamebratu>
2024-08-17 10:20:41 +0300

    update file

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -13,12 +13,12 @@ urlpatterns = [
 ]
 # relationship_app/urls.py
 urlpatterns = [
-    # URL pattern for user login
-    path('login/', auth_views.LoginView.as_view(template_name = 'registration/login.html'), name ='login'),
-    
+    # URL pattern for user login with custom template
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+
     # URL pattern for user logout
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name = 'logout'),
-    
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
     # URL pattern for user registration
     path('register/', register, name='register'),
 ]
