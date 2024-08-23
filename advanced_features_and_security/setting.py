b8709151147152a14ce6ab89b6eec6145cee4e30
girmameb <girmamebratu>
2024-08-23 22:26:17 +0300

    update file

--- advanced_features_and_security/setting.py
+++ advanced_features_and_security/setting.py
@@ -1,1 +1,11 @@
+# advanced_features_and_security/settings.py
+
+# Add 'accounts' app to INSTALLED_APPS
+INSTALLED_APPS = [
+    # other apps
+    'accounts',
+    # ...
+]
+
+# Set the custom user model
 AUTH_USER_MODEL = 'accounts.CustomUser'
