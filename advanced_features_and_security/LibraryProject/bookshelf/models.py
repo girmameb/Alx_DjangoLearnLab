a936956e594fe63759b6e2b3c8e22df168183ac3
girmameb <girmamebratu>
2024-08-24 01:08:37 +0300

    update file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,15 +1,10 @@
 # bookshelf/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from .managers import CustomUserManager
-
+from .managers import CustomUserManager  # Import the custom manager
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    objects = CustomUserManager()
-
-    def some_method(self):
-        # Proper indentation
-        pass
+    objects = CustomUserManager()  # Assign the custom manager
