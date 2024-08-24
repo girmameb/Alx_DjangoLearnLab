ff9f3fd7ef3bb2609f2288187602f41554ab00df
girmameb <girmamebratu>
2024-08-24 15:14:58 +0300

    update file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+# advanced_features_and_security/bookshelf/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Article
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'bookshelf/article_detail.html', {'article': article})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_article(request):
+    if request.method == 'POST':
+        # Handle form submission
+        # Save the new article
+        return redirect('article_list')
+    return render(request, 'bookshelf/article_form.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'POST':
+        # Handle form submission
+        # Update the article
+        return redirect('article_detail', article_id=article.id)
+    return render(request, 'bookshelf/article_form.html', {'article': article})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('article_list')
+    return render(request, 'bookshelf/article_confirm_delete.html', {'article': article})
