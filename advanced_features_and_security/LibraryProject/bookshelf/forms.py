1c5764df0708a3e48ebdd1477993b2866f9244e9
girmameb <girmamebratu>
2024-08-24 16:34:27 +0300

    update file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,14 +2,14 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
 
     def clean_title(self):
         title = self.cleaned_data.get('title')
-        # Example validation: Ensure title is not too short
+        # Ensure title is not too short
         if len(title) < 3:
             raise forms.ValidationError("Title must be at least 3 characters long.")
         return title
