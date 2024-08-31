bb6046638454061a76d678180c034a32d63c9b60
girmameb <girmamebratu>
2024-08-31 17:25:50 +0300

    update file

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,12 @@
 # api/urls.py
 
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Define the endpoint URL
+    path('', include(router.urls)),  # Automatically includes all the URL patterns for the viewset
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
 # api/views.py
 
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(generics.ListAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
