820eb0b29950836db5bcfeac8fc2c698e3d8a9de
girmameb <girmamebratu>
2024-09-09 01:23:08 +0300

    update file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,11 @@
+import django_filters
+from django_filters import DjangoFilterBackend
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from django_filters import rest_framework
-from django_filters import DjangoFilterBackend
-import django_filters
+
 from .models import Book
 from .serializers import BookSerializer
+
 
 class BookFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
