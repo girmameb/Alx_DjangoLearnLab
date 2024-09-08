92b60c6c8bf94ac597b33fa8046c08c6039a8952
girmameb <girmamebratu>
2024-09-09 00:43:12 +0300

    update file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List all books and create a new one
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Explicit create URL
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve, update, or delete a specific book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Explicit update URL
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Explicit delete URL
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # Explicit update URL
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # Explicit delete URL
 ]
