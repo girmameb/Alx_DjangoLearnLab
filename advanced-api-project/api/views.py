195b940de855811050222f06199892faa8fd59e9
girmameb <girmamebratu>
2024-09-09 00:52:23 +0300

    update file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
-from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
 
 
 class BookListView(generics.ListCreateAPIView):
