fed6b0e8fd85a22ed3310a49b272569a0f31c0fe
girmameb <girmamebratu>
2024-09-09 01:14:49 +0300

    update file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
+from django_filters import DjangoFilterBackend
 import django_filters
 from .models import Book
 from .serializers import BookSerializer
