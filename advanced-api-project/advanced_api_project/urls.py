cfa497db33b900b23a556c83063a2e09d7279f6f
girmameb <girmamebratu>
2024-09-09 00:57:09 +0300

    update file

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Admin interface
+    path('api/', include('api.urls')),  # API routes
 ]
