83625174dba7b8fbd1cb2299e927a9a8c9aa2c58
girmameb <girmamebratu>
2024-09-21 22:52:37 +0300

    update file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
