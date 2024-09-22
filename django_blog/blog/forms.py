3d05f62c2d0adc1c03c2197016734f2d9e682a19
girmameb <girmamebratu>
2024-09-22 23:21:20 +0300

    update file

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django_blog.blog.models import Comment, Post, Tag
+from .models import Comment, Post  # Ensure you're importing your Post model
+from taggit.models import Tag
 
-
+# Custom user creation form
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -18,19 +19,13 @@ class CustomUserCreationForm(UserCreationForm):
             user.save()
         return user
 
-from django import forms
-from .models import Comment
-
-
+# Comment form
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']  # Only allow content to be set
-from django import forms
-from .models import Post
-from taggit.models import Tag
 
-
+# Post form with tagging functionality
 class PostForm(forms.ModelForm):
     tags = forms.CharField(
         max_length=200,
@@ -42,3 +37,17 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'tags']
 
+    def clean_tags(self):
+        tags_input = self.cleaned_data.get('tags')
+        if tags_input:
+            tags = [tag.strip() for tag in tags_input.split(',')]
+            return tags
+        return []
+
+    def save(self, commit=True):
+        post = super().save(commit)
+        for tag in self.cleaned_data['tags']:
+            post.tags.add(tag)  # Assuming Post model uses TaggableManager
+        if commit:
+            post.save()
+        return post
